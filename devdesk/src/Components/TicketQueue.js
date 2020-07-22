4de3163a02648d349f01dff8955106c239da0e73
Mitchell Wright <mitchellbwright@gmail.com>
2020-07-21 19:43:46 -0600

    building out page routes and initial components

---
+++ devdesk/src/Components/TicketQueue.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TicketQueue = (props) => {
+
+    return (
+        <div>
+            <h1>TicketQueue page</h1>
+        </div>
+    );
+};
+
+export default TicketQueue;
