22512d05c9dad87e2ed08a8c07d78dfe7be36a25
Jason Sarrio <jsarrio16@gmail.com>
2020-07-22 12:07:19 -0500

    Working on Small changes

---
+++ devdesk/src/component/Student/CreateTicket.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
+import { Link } from "react-router-dom";
+
+
+// Createing Ticket Header an Styles 
+
+
+
+
+
+
+
+export default function CreateTicket() {
+    const [category, setCategory] = useState([]);
+  
+    const history = useHistory();
+    useEffect(() => {
+      Axios.get("API")
+      .then(res => {
+          console.log(res.data);
+          setCategory(res.data);
+        })
+        .catch(e => console.log(e.message))
+        .finally(() => {
+          console.log("Axios request finished.");
+        });
+    }, []);
+  
+    function submitHandler(values, actions) {
+      console.log(values, actions);
+      values["status"] = "Open";
+      values["student_id"] = localStorage.getItem("userId");
+      // Sending form data to server
+      axios
+        .post("API", values)
+        .then(res => {
+          console.log(res);
+          if (res.status === 200) {
+            history.push("/student/dashboard");
+          }
+          console.log("response", res);
+          actions.resetForm();
+        })
+        .catch(e => console.log(e.message))
+        .finally(() => {
+          console.log("Axios request finished.");
+        });
+    }
+  
+    return (
+      <>
+        <MainHeader>
+          <Title>
+            <Img
+              className="main-img"
+              src={require(`./Lambda_Logo.jpg`)}
+              alt="logo"
+            />
+            <h1>DevDesk</h1>
+          </Title>
+          <Nav>
+            <Link className="nav-links" to={"/student/dashboard"}>
+              Dashboard
+            </Link>
+            <Link className="nav-links" to={"/login"}>
+              Sign Out
+            </Link>
+          </Nav>
+        </MainHeader>
+
+
+        
+
+        //Form Formik 
+        
+
+
+
+
+
+
+
+
