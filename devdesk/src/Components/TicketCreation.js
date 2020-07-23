36ec2be8392f56b488c88a32884f51bb3f2abdf5
Mitchell Wright <mitchellbwright@gmail.com>
2020-07-22 20:15:12 -0600

    add basic styling

--- devdesk/src/Components/TicketCreation.js
+++ devdesk/src/Components/TicketCreation.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import Select from '@material-ui/core/Select';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    button: {
+      marginTop: "32px",
+      backgroundColor: "#00B4D8",
+      "&:hover": {
+        boxShadow: "none",
+        backgroundColor: "#0077B6"
+      },
+    },
+    refreshIcon: {
+      fontSize: "16px",
+      marginRight: "6px",
+    },
+    paper: {
+      boxShadow: "2px 4px 10px rgba(0, 0, 0, 0.02), 2px 3px 8px rgba(0, 0, 0, 0.02)",
+      background: "#FFFFFF",
+      bordeRadius: "4px",
+      width: "343px",
+      marginTop: "32px"
+    },
+});
 
 const TicketCreation = (props) => {
     const { register, handleSubmit, control } = useForm();
     const submitHandler = data => console.log(data);
+    const classes= useStyles();
 
     return (
         <div>
@@ -22,6 +44,7 @@ const TicketCreation = (props) => {
                         variant="outlined"
                         inputRef={register}
                         name='title'
+                        className={classes.paper}
                     />
                 </div>
                 <div>
@@ -30,6 +53,7 @@ const TicketCreation = (props) => {
                         <Select
                             label="What is this issue about?"
                             variant="outlined"
+                            className={classes.paper}
                         >
                             <MenuItem value='react'>React</MenuItem>
                             <MenuItem value='javascript'>JavaScript</MenuItem>
@@ -51,6 +75,7 @@ const TicketCreation = (props) => {
                         variant="outlined"
                         name="tried"
                         inputRef={register}
+                        className={classes.paper}
                     />
                 </div>
                 <div>
@@ -62,16 +87,18 @@ const TicketCreation = (props) => {
                         variant="outlined"
                         name="notes"
                         inputRef={register}
+                        className={classes.paper}
                     />
                 </div>
                 <div>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                >
-                    Submit Ticket
-                </Button>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        type="submit"
+                        className={classes.button}
+                    >
+                        Submit Ticket
+                    </Button>
                 </div>
             </form>
         </div>
