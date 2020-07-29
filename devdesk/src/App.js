a3b70abe71b2802ea9169a0c8d0aff1e28546540
Mitchell Wright <mitchellbwright@gmail.com>
2020-07-28 18:49:49 -0600

    update so sidebar only renders on logged in pages

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -10,22 +10,17 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import CreateIcon from '@material-ui/icons/Create';
 import InboxIcon from '@material-ui/icons/Inbox';
-import Login from "./Components/Login";
+
 
 // Custom component imports
-// import Login from './Components/Login';
+import Sidebar from "./Components/Sidebar";
+import Login from "./Components/Login";
 import Register from './Components/Register';
 import TicketCreation from './Components/TicketCreation';
 import TicketQueue from './Components/TicketQueue';
 import PrivateRoute from './Components/PrivateRoute';
-import { AvMicNone } from 'material-ui/svg-icons';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -33,13 +28,9 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     backgroundColor: "#0077B6"
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
   title: {
     flexGrow: 1,
   },
-  toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
@@ -53,10 +44,10 @@ const useStyles = makeStyles((theme) => ({
 
 function App(props) {
   const classes = useStyles();
-  const [links, setLinks] = useState([
+  const links = [
     {title:'Create Ticket', url: '/create-ticket', icon: <CreateIcon />},
     {title: 'View Tickets', url: '/ticket-queue', icon: <InboxIcon />},
-  ]);
+  ];
   const location = useLocation();
   const [pageTitle, setPageTitle] = useState("");
 
@@ -88,35 +79,12 @@ function App(props) {
       {/* Top navigation bar */}
       <AppBar position="static" className={classes.root}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             {pageTitle}
           </Typography>
           <Link to="/login"><Button color="inherit">Login</Button></Link>
         </Toolbar>
       </AppBar>
-      {/* Sidebar for navigation and/or ticket sorting purposes */}
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        anchor="left"
-      >
-        <div className={classes.toolbar} />
-        <List>
-          {links.map((link, index) => (
-            <Link to={link.url} key={index} className={classes.link}>
-              <ListItem button key={link.title}>
-                <ListItemIcon>
-                  {link.icon}
-                </ListItemIcon>
-                <ListItemText primary={link.title} />
-              </ListItem>
-            </Link>
-          ))}
-        </List>
-      </Drawer>
       {/* Switch for routing */}
       <Switch>
         <Route exact path='/'>
@@ -129,14 +97,17 @@ function App(props) {
           <Register />
         </PrivateRoute>
         <PrivateRoute exact path='/create-ticket'>
+          <Sidebar links={links} />
           {/* //add ticket creation path */}
           <TicketCreation />
         </PrivateRoute>
         <PrivateRoute exact path='/students/:id/tickets/'>
+          <Sidebar links={links} />
           {/* //add ticket queue path */}
           <TicketQueue />
         </PrivateRoute>
         <PrivateRoute exact path='/helpers/:id/tickets/'>
+          <Sidebar links={links} />
           {/* //add ticket queue path */}
           <TicketQueue />
         </PrivateRoute>
