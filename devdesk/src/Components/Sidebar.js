1d9bb5e3533d62dea10ec108c9aaff6af3d1e49f
Jeff Glanville <jeff.glanville@outlook.com>
2020-07-30 20:20:07 -0500

    remove useParams form sidebar

--- devdesk/src/Components/Sidebar.js
+++ devdesk/src/Components/Sidebar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -24,7 +23,6 @@ const useStyles = makeStyles((theme) => ({
 
 const Sidebar = ({ links }) => {
     const classes = useStyles();
-    const params = useParams();
 
     return (
         <Drawer
